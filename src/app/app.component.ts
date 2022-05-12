b682bc1d4c544bc8c71061f9ac266458bcf42aec
Tharnas <8459274+Tharnas@users.noreply.github.com>
2022-05-12 14:11:03 +0200

    implement checked list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSelectionListChange } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'DemoCheckedList';
+  products: { name: string; id: number }[] = [
+    { name: 'Apple', id: 1 },
+    { name: 'Orange', id: 2 },
+    { name: 'Banana', id: 3 },
+    { name: 'Pear', id: 4 },
+  ];
+  public productsForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.productsForm = fb.group({
+      products: [''],
+    });
+  }
+
+  public selectionChanged(event: MatSelectionListChange): void {
+    const unselected = event.options
+      .filter((x) => x.selected === false)
+      .map((x) => x.value);
+
+    const newSelection = (<{ name: string; id: number }[]>(
+      this.productsForm.value.products
+    )).filter((x) => !unselected.some((y) => y.id === x.id));
+
+    this.productsForm.patchValue({ products: newSelection });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatListModule } from '@angular/material/list';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
